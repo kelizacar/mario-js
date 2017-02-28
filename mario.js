0378c274f81f8f41ed48832cb078c43d5ac2b323
Kelley Carpenter <kelleyecarpenter@gmail.com>
2017-02-27 19:58:54 -0500

    Backwards Mario Pyramid

--- mario.js
+++ mario.js
@@ -13,11 +13,7 @@ printPyramid(5);
  *       #####
  *      ######
  */
-function printPyramid(height) {
-    console.log("Uh oh... the pyramid is under construction.");
-    console.log("Check back soon, our developers are hard at work as we speak!");
-
-    // TODO
-    // print that pyramid!
-
-}
+ function printPyramid(height) {
+ for (var brick = "##"; brick.length <= height + 1; brick+="#")
+ console.log(brick)
+ }
